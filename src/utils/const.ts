85d73a039de118cc5f5e590c0110154f36ebf28f
Ivan <leosnchz.ilst@gmail.com>
2025-06-06 18:59:11 -0500

    REF: SQL user

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,7 +1,5 @@
-export const updTarea = `UPDATE tareas SET usuario_asignado_id = ?, estado = ?, titulo = ?, descripcion = ?, fecha_vencimiento = ? WHERE tarea_id = ?`;
-export const delTarea = `DELETE FROM tareas WHERE tarea_id = ?`;
-export const insTarea = `INSERT INTO tareas (usuario_asignado_id, usuario_creador_id, estado, titulo, descripcion, fecha_vencimiento) VALUES (?, ?, ?, ?, ?, ?)`;
-export const getUsersByStatus = `SELECT id, name, email, status, createdAt FROM users WHERE status = ?`;
-export const getUsr = `SELECT usuario_id, nombre, usuario, estado FROM usuario`;
-export const getOptByUser = `select o.name, o.path, o.icon, o.parent_id from roles_users rs join roles r on rs.role_id = r.id join roles_options ro on r.id = ro.role_id join options o on ro.option_id = o.id WHERE rs.user_id = ? group by o.id`;
+export const getUsersByStatus = `SELECT id, name, email, status FROM users WHERE status = ?`;
 
+export const getUsr = `SELECT u.name as username, email, d.name as department, u.status,  u.createdAt FROM  users u join department d on u.department_id = d.id`;
+
+export const getOptByUser = `select o.icon,o.name as label,o.path, o.parent_id from roles_users rs join roles r on rs.role_id = r.id join roles_options ro on r.id = ro.role_id join options o on ro.option_id = o.id WHERE rs.user_id = ? group by o.id`;
