e637bdee71cb08cae0663f3ae4b97a9285953434
Ivan <leosnchz.ilst@gmail.com>
2025-06-06 18:55:47 -0500

    ADD: Manejo de validadores de request

---
+++ src/middleware/validationHandler.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import { sendResponse } from '../utils/response';
+
+
+export const handleValidationErrors =
+(returnUnauthorized = false) => (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+  // console.log(errors);
+  if (!errors.isEmpty()) {
+    const status = returnUnauthorized ? 401 : 400;
+    return sendResponse(res, status, 'error', 'Datos inválidos.', errors.array());
+  }
+  next();
+};
